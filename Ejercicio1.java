19fedc217464ce831d706db34f2e2c1cdf7abadb
maxiilandriel12@gmail.com <129111730+lwaxil@users.noreply.github.com>
2023-08-11 21:44:26 -0300

    Commit message

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Ejercicio1 {
     public static void main (String args[]) {
         Scanner scanner=new Scanner(System.in);
-        int mayor;
+        int mayor, medio, menor;
         System.out.println("Ingrese el primer numero");
         int num1= scanner.nextInt();
         System.out.println("Ingrese el segundo numero");
@@ -12,12 +12,33 @@ public class Ejercicio1 {
         int num3= scanner.nextInt();
         if (num1>num2 && num1>num3) {
             mayor=num1;
+            if (num2>num3) {
+                medio=num2;
+                menor=num3;
+            } else {
+                medio=num3;
+                menor=num2;
+            }
         } else if (num2>num1 && num2>num3) {
             mayor=num2;
+            if (num1>num3) {
+                medio=num1;
+                menor=num3;
+            } else {
+                medio=num3;
+                menor=num1;
+            }
         } else {
             mayor=num3;
+            if (num2>num1) {
+                medio=num2;
+                menor=num1;
+            } else {
+                medio=num1;
+                menor=num2;
+            }
         }
-        System.out.println("El mayor es " + mayor);
+        System.out.println("El mayor es " + mayor+" El del medio es "+medio+" El menor es "+menor);
 
     }
 }
