f2fd5faddc3b4a0c543cdc7961b4ba23ed4ab496
maxiilandriel12@gmail.com <129111730+lwaxil@users.noreply.github.com>
2023-08-18 21:30:35 -0300

    Commit message

--- src/Recursion/Programa_1.java
+++ src/Recursion/Programa_1.java
@@ -6,36 +6,33 @@ import java.util.Scanner;
 Implementa ambos enfoques en la misma clase, utilizando la sobrecarga de métodos para diferenciarlos. Luego, realiza pruebas utilizando distintos pares de números.*/
 public class Programa_1 {
     // Enfoque recursivo
-    public static int Recursivo(int dividendo, int divisor) {
+    public static int Division(int dividendo, int divisor) {
         if (dividendo < divisor) {
             return 0;
         }
-        return 1 + Recursivo(dividendo - divisor, divisor);
+        return 1 + Division(dividendo - divisor, divisor);
     }
 
     // Enfoque iterarivo
-    public static int Iteracion(int dividendo, int divisor) {
-        int contador = 0;
+    public static int Division(int dividendo, int divisor, int contador) {
         while (dividendo >= divisor) {
             dividendo = dividendo - divisor;
             contador++;
         }
-
         return contador;
     }
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
+        int cont = 0;
 
         System.out.println("Ingrese el dividendo");
         int dividendo = scanner.nextInt();
         System.out.println("Ingrese el divisor (debe ser mayor que 0)");
         int divisor = scanner.nextInt();
-
-        int resultadoRecursivo = Recursivo(dividendo, divisor);
-        System.out.println("El resultado usando recursion: " + resultadoRecursivo);
-
-        int resultadoIterativo = Iteracion(dividendo, divisor);
-        System.out.println("El resultado usando iteracion: " + resultadoIterativo);
+        
+        System.out.println("El resultado usando recursion: " + Division(dividendo, divisor));
+        
+        System.out.println("El resultado usando iteracion: " + Division(dividendo, divisor, cont));
     }
 }
