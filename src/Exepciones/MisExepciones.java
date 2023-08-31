8f69e27efc7a0c91d1dbf14b8402e575750ecf74
Maximiliano Landriel <129111730+lwaxil@users.noreply.github.com>
2023-08-31 17:46:02 -0300

    Commit message

---
+++ src/Exepciones/MisExepciones.java
@@ -0,0 +1,37 @@
+package Exepciones;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class MisExepciones {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Introduce un numero: ");
+        Scanner leer = new Scanner(System.in);
+
+        try {
+            evaluarNumero(leer.nextInt());
+        } catch (LongitudDelNumero e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("Fin del programa");
+    }
+
+    static void evaluarNumero(int nroEvaluar) throws LongitudDelNumero {
+        if (nroEvaluar <= 100 && nroEvaluar >= 1) {
+            LongitudDelNumero miPropiaExcepcion = new LongitudDelNumero("El número esta dentro del rango");
+            throw miPropiaExcepcion;
+        }
+    }
+}
+
+class LongitudDelNumero extends IOException {
+    public LongitudDelNumero() {
+    }
+    public LongitudDelNumero(String mensajeError) {
+        super(mensajeError);
+    }
+}
